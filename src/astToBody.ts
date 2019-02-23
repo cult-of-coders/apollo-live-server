9fb903dd4ef45329fb653921dafefa03a5e5fe07
moberegger <moberegger@hotmail.com>
2019-02-23 15:37:14 -0500

    Rewrite astToFields to support fragments in queries

--- src/astToBody.ts
+++ src/astToBody.ts
@@ -1,33 +1,12 @@
-export default function astToBody(ast) {
-  const fieldNodes = ast.fieldNodes;
+import graphqlFields from 'graphql-fields';
+import isEmpty from 'lodash/isEmpty';
+import mapValues from 'lodash/mapValues';
 
-  const body = extractSelectionSet(ast.fieldNodes[0].selectionSet);
-
-  return body;
-}
+const toMongoFieldSet = (fields = {}) =>
+  mapValues(fields, fieldValue =>
+    isEmpty(fieldValue) ? 1 : toMongoFieldSet(fieldValue)
+  );
 
 export function astToFields(ast) {
-  return astToBody(ast).doc;
-}
-
-/**
- * @param set
- */
-function extractSelectionSet(set): any {
-  let body = {};
-  set.selections.forEach(el => {
-    if (!el.selectionSet) {
-      body[el.name.value] = 1;
-    } else {
-      body[el.name.value] = extractSelectionSet(el.selectionSet);
-      if (el.arguments.length) {
-        let argumentMap = {};
-        el.arguments.forEach(arg => {
-          argumentMap[arg.name.value] = arg.value.value;
-        });
-      }
-    }
-  });
-
-  return body;
+  return toMongoFieldSet(graphqlFields(ast).doc);
 }
